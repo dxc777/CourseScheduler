51f425c36fd38e539acb424bdb940b99301721e6
JG <ladrenrights@gmail.com>
2022-12-12 20:56:11 -0800

    Added state type enum that specifies if state is error or success

---
+++ src/StateType.java
@@ -0,0 +1,7 @@
+
+public enum StateType
+{
+	ERROR,
+	SUCCESS,
+	NEUTRAL
+}
--- src/States.java
+++ src/States.java
@@ -2,27 +2,30 @@
 public enum States
 {
 	MAX_UNITS_REACHED(0,"The max units for this semester have already been reached either remove"
-			+ "a class or move this class to the next semester"), 
-	CLASS_EXCEEDS_MAX_UNITS(1,"Adding this class will exceed the set max units"), 
-	AT_UNIT_MAX(2,"This class has been added and the max units have now been reached"), 
-	CLASS_ADDED(3,"This class has been added and the max units have not been reached"),
-	NEGATIVE_SEMESTER(4,"You cannot move to a negative semester"), 
-	SEMESTER_CHANGED(5,"The semester has been changed"),
-	INSUFFICIENT_DATA(6,"Line does have minimum amount of data needed to parse:"),
-	NAN(7,"The units field cannot be parsed"),
-	REUSED_IDENTIFIER(8,"Class identifiers cannot be reused"),
-	FILE_NOT_FOUND(9,"The entered file cannot be found"),
-	INVALID_INDEX(10,"The list number entered is not valid enter again"),
-	UNDECLARED_IDENTIFIER(11,"This identifier has not been declared and is being used");
+			+ "a class or move this class to the next semester",StateType.ERROR), 
+	CLASS_EXCEEDS_MAX_UNITS(1,"Adding this class will exceed the set max units",StateType.ERROR), 
+	AT_UNIT_MAX(2,"This class has been added and the max units have now been reached",StateType.SUCCESS), 
+	CLASS_ADDED(3,"This class has been added and the max units have not been reached",StateType.SUCCESS),
+	NEGATIVE_SEMESTER(4,"You cannot move to a negative semester",StateType.ERROR), 
+	SEMESTER_CHANGED(5,"The semester has been changed",StateType.SUCCESS),
+	INSUFFICIENT_DATA(6,"Line does have minimum amount of data needed to parse:",StateType.ERROR),
+	NAN(7,"The units field cannot be parsed",StateType.ERROR),
+	REUSED_IDENTIFIER(8,"Class identifiers cannot be reused",StateType.ERROR),
+	FILE_NOT_FOUND(9,"The entered file cannot be found",StateType.ERROR),
+	INVALID_INDEX(10,"The list number entered is not valid enter again",StateType.ERROR),
+	UNDECLARED_IDENTIFIER(11,"This identifier has not been declared and is being used",StateType.ERROR);
 	
 	private final int STATE_CODE;
 	
 	private final String MESSAGE;
 	
-	private States(int STATE_CODE,String MESSAGE) 
+	private final StateType stateType;
+	
+	private States(int STATE_CODE,String MESSAGE, StateType stateType) 
 	{
 		this.STATE_CODE = STATE_CODE;
 		this.MESSAGE = MESSAGE;
+		this.stateType = stateType;
 	}
 	
 	public int getStateCode() 
@@ -33,6 +36,11 @@ public enum States
 	public String getMessage() 
 	{
 		return MESSAGE;
+	}
+	
+	public StateType getType() 
+	{
+		return stateType;
 	}
 	
 
