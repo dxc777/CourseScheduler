4b91dc820c7cbe05108e4679ef3b11f81fc5b1d5
JG <ladrenrights@gmail.com>
2022-12-31 16:01:21 -0800

    filterData now allows '-' character

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -15,7 +15,7 @@ import java.util.Queue;
  * TODO: Add support for classes all under 1 topic where user pick x amount of classses from topic
  * TODO: Add support for classes that are only available during certain terms i.e fall or sping
  * TODO: Add tags that can be used to differentiate between general education or major class
- * TODO: Add ability to change max units while planning
+ * 
  * @author J
  *
  */
